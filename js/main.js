2511af0cdc63df2ca1e1df6ec42d81fc6e4e1131
dulcetiseduce1 <mariusdulce@tuta.io>
2022-05-17 15:05:55 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito22
+
+const inputName = prompt("Inserisci il tuo nome")
+const inputSurname = prompt("Inserisci il tuo cognome")
+const inputColor = prompt("Inserisci il tuo colore preferito")
+const outputJS = document.getElementById("outputHTML")
+outputJS.innerHTML = `<strong>
+                    ${inputName} ${inputSurname} ${inputColor}
+                    22
+                    </strong>`
